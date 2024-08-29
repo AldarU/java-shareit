5760b5f62dd20244d594957953d0123b05f53fbb
Aldar <aldarubushieff@yandex.ru>
2024-08-29 23:38:05 +0400

    update: добавлена вторая реализация проекта shareIt

---
+++ src/test/java/ru/practicum/shareit/booking/BookingTest.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BookingTest {
+    @Test
+    void createBookingWhenTimeIsNotValidThenReturnedTimeDataException() {
+        Assertions.assertTrue("".equals(""));
+    }
+}
