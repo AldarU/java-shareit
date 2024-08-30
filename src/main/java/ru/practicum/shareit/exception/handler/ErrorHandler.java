5b0741f54f9311a5bae6095585db553c9d95acbb
Aldar <aldarubushieff@yandex.ru>
2024-08-30 23:51:33 +0400

    update: добавлена вторая реализация проекта shareIt

--- src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
@@ -1,8 +1,30 @@
 package ru.practicum.shareit.exception.handler;
 
+import io.micrometer.core.instrument.config.validate.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleUserNotFoundException(final Exception e) {
+        return Map.of("error", e.getMessage());
+    }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(final ValidationException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> handleUserAlreadyExisxtsException(final Exception e) {
+        return Map.of("error", e.getMessage());
+    }
 }
