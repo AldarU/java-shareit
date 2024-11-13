1e89884dbfc1e8d2aa75da96648ae904eef9b06b
Aldar <aldarubushieff@yandex.ru>
2024-11-13 22:39:05 +0400

    update: новый спринт

--- shareIt-server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
+++ shareIt-server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.*;
-import org.springframework.http.MediaType;
 import ru.practicum.shareit.request.controller.ItemRequestController;
 import ru.practicum.shareit.request.dto.RequestDto;
 import ru.practicum.shareit.request.service.RequestServiceImpl;
